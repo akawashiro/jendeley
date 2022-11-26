8a87e3688bbb9f792d9be1ee3f36ecfd301fffc6
Akira Kawata <akirakawata1@gmail.com>
2022-11-26 22:53:12 +0900

    DB construction finished

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {Command} from 'commander';
 import fs from 'fs';
 import path from 'path';
 import pdfparse from 'pdf-parse';
+import node_isbn from 'node-isbn'
 
 
 function walkPDF(dir: string): Array<string> {
@@ -21,48 +22,110 @@ function walkPDF(dir: string): Array<string> {
     }
 }
 
-async function getDOI(pdf: string) {
+async function getID(pdf: string) {
     let dataBuffer = fs.readFileSync(pdf);
 
-    const text = await pdfparse(dataBuffer).then(data => {
+    const texts = await pdfparse(dataBuffer).then(data => {
         // See https://www.npmjs.com/package/pdf-parse for usage
-        return data.text;
+        return data.text.split(/\r?\n/);;
     }).catch((e) => {
-        console.error(e.message);
-        return null;
+        console.warn(e.message);
+        return {"doi": null, "isbn": null};
     });
 
-    if (text == null) {
-        return null;
+    if (texts == null) {
+        return {"doi": null, "isbn": null};
     }
 
-    const regexp = '(10[.][0-9]{2,}(?:[.][0-9]+)*/(?:(?![%"#? ])\\S)+)';
-    const found = [...text.matchAll(regexp)];
+    const regexpDOI = '(10[.][0-9]{2,}(?:[.][0-9]+)*/(?:(?![%"#? ])\\S)+)';
+    const regexpISBN = "(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]";
 
-    for (const f of found) {
-        let d = f[0] as string;
-        if (d.charAt(d.length - 1) == '.') {
-            d = d.substr(0, d.length - 1);
+    let doi: string | null = null
+    for (const text of texts) {
+        const foundDOI = [...text.matchAll(regexpDOI)];
+        for (const f of foundDOI) {
+            let d = f[0] as string;
+            if (d.charAt(d.length - 1) == '.') {
+                d = d.substr(0, d.length - 1);
+            }
+            doi = d;
+            break
         }
-        return d
     }
 
-    return null
+    let isbn: string | null = null
+    for (const text of texts) {
+        const foundISBN = [...text.matchAll(regexpISBN)];
+        for (const f of foundISBN) {
+            let d = f[0] as string;
+            let n = '';
+            for (const c of d) {
+                if (('0' <= c && c <= '9') || c == 'X') {
+                    n += c;
+                }
+            }
+            if (n.length == 10) {
+                let cd = 0;
+                for (let i = 0; i < 9; i++) {
+                    cd += (10 - i) * (n.charCodeAt(i) - '0'.charCodeAt(0));
+                }
+                cd = 11 - cd % 11;
+                const cd_c = cd == 10 ? 'X' : String.fromCharCode('0'.charCodeAt(0) + cd);
+                if (cd_c == n[9]) {
+                    isbn = n;
+                }
+            } else if (n.length == 13) {
+                let cd = 0;
+                for (let i = 0; i < 12; i++) {
+                    if (i % 2 == 0) {
+                        cd += (n.charCodeAt(i) - '0'.charCodeAt(0));
+                    } else {
+                        cd += (n.charCodeAt(i) - '0'.charCodeAt(0)) * 3;
+                    }
+                }
+                cd = 10 - (cd % 10);
+                const cd_c = String.fromCharCode('0'.charCodeAt(0) + cd);
+                if (cd_c == n[12]) {
+                    isbn = n;
+                }
+            }
+            break
+        }
+    }
+
+
+    return {"doi": doi, "isbn": isbn};
 }
 
-async function getJSON(doi: string) {
+async function getDoiJSON(doi: string) {
     let {got} = await import('got');
 
     const URL = 'https://api.crossref.org/v1/works/' + encodeURIComponent(doi) + "/transform"
     const options = {'headers': {'Accept': 'application/json'}}
-    const data = await got(URL, options).json();
-    return data
+    try {
+        const data = await got(URL, options).json();
+        return data
+    } catch {
+        console.warn("Failed to get information from ", URL)
+        return new Object();
+    }
+}
+
+async function getIsbnJson(isbn: string) {
+    const b = await node_isbn.resolve(isbn).then(function (book) {
+        console.log('Book found %j', book);
+        return book;
+    }).catch(function (err) {
+        console.log('Book not found', err);
+    });
+    return b;
 }
 
 async function main() {
     const program = new Command();
     program
         .requiredOption('--papers_dir <dir>', "Root directory of your papers")
+        .option('--output <out>', "Output DB to this file. By default, <papers_dir>/db.json.")
     program.parse();
     const options = program.opts();
 
@@ -73,26 +136,32 @@ async function main() {
 
         let pdfs = walkPDF(options.papers_dir);
         pdfs.sort();
-        pdfs = pdfs.slice(0, 10);
-        console.log(pdfs)
+        // pdfs = pdfs.slice(0, 10);
         for (const p of pdfs) {
             console.log("Processing ", p)
-            const doi = await getDOI(p);
-            if (doi != null) {
-                const json = await getJSON(doi);
-                json_db["doi_" + doi.replace(".", "_").replace("/", "_")] = json
+            const id = await getID(p);
+            if (id["doi"] != null) {
+                const json = await getDoiJSON(id["doi"]);
+                json_db["doi_" + id["doi"].replace(".", "_").replace("/", "_")] = json
                 if (json != null) {
-                    console.log(json, p)
+                    console.log(json["title"], p)
+                }
+            } else if (id["isbn"] != null) {
+                const json = await getIsbnJson(id["isbn"]);
+                json_db["isbn_" + id["isbn"]] = json
+                if (json != null) {
+                    console.log(json["title"], p)
                 }
             } else {
-                console.warn("Failed to get DOI of", p)
+                console.warn("Failed to get DOI or ISBN of", p)
             }
         }
 
         try {
-            fs.writeFileSync(path.join(options.papers_dir, "db.json"), JSON.stringify(json_db));
+            const db_path = options.output == undefined ? path.join(options.papers_dir, "db.json") : options.output;
+            fs.writeFileSync(db_path, JSON.stringify(json_db));
         } catch (err) {
-            console.error(err);
+            console.warn(err);
         }
     } else {
         console.log(options.papers_dir + " is not exist.");
