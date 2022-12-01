ad3dbd77ed300b5f1351ac15c5856f27c974cf95
Akira Kawata <akirakawata1@gmail.com>
2022-12-01 21:53:26 +0900

    Hide column

--- frontend/jendeley-front-type/src/App.tsx
+++ frontend/jendeley-front-type/src/App.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import base_64 from 'base-64'
 import './App.css';
 import {Entry, DB} from './schema';
+// import path_lib from 'path'
 
 import {
     createColumnHelper,
@@ -10,6 +11,7 @@ import {
     getCoreRowModel,
     useReactTable,
 } from '@tanstack/react-table'
+
 
 const columnHelper = createColumnHelper<Entry>()
 
@@ -59,42 +61,79 @@ function App() {
     }, []);
 
 
+    const [columnVisibility, setColumnVisibility] = React.useState({})
     const table = useReactTable({
         data,
         columns,
+        state: {
+            columnVisibility,
+        },
+        onColumnVisibilityChange: setColumnVisibility,
         getCoreRowModel: getCoreRowModel(),
     })
 
     return (
-        <table id="jendeley_main_table">
-            <thead>
-                {table.getHeaderGroups().map(headerGroup => (
-                    <tr key={headerGroup.id}>
-                        {headerGroup.headers.map(header => (
-                            <th key={header.id}>
-                                {header.isPlaceholder
-                                    ? null
-                                    : flexRender(
-                                        header.column.columnDef.header,
-                                        header.getContext()
-                                    )}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody>
-                {table.getRowModel().rows.map(row => (
-                    <tr key={row.id}>
-                        {row.getVisibleCells().map(cell => (
-                            <td key={cell.id}>
-                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                            </td>
-                        ))}
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <div>
+            <div className="inline-block border border-black shadow rounded">
+                <div className="px-1 border-b border-black">
+                    <label>
+                        <input
+                            {...{
+                                type: 'checkbox',
+                                checked: table.getIsAllColumnsVisible(),
+                                onChange: table.getToggleAllColumnsVisibilityHandler(),
+                            }}
+                        />{' '}
+                        Toggle All
+                    </label>
+                </div>
+                {table.getAllLeafColumns().map(column => {
+                    return (
+                        <div key={column.id} className="px-1">
+                            <label>
+                                <input
+                                    {...{
+                                        type: 'checkbox',
+                                        checked: column.getIsVisible(),
+                                        onChange: column.getToggleVisibilityHandler(),
+                                    }}
+                                />{' '}
+                                {column.id}
+                            </label>
+                        </div>
+                    )
+                })}
+            </div>
+            <table id="jendeley_main_table">
+                <thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
