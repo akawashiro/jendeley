7a209e940d2caa46d2732eca6262cac7a1c05803
Akira Kawata <akirakawata1@gmail.com>
2022-12-08 07:26:32 +0900

    Color authors

--- jendeley-frontend/src/App.tsx
+++ jendeley-frontend/src/App.tsx
@@ -9,13 +9,75 @@ import MaterialReactTable, {
   MRT_ColumnDef,
 } from "material-react-table";
 import sanitizeHTML from "sanitize-html";
+import {
+  red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
+  blue,
+  lightBlue,
+  cyan,
+  teal,
+  green,
+  lightGreen,
+  lime,
+  yellow,
+  amber,
+  orange,
+  deepOrange,
+  brown,
+  grey,
+  blueGrey,
+} from "@mui/material/colors";
+
+function hashString(s: string) {
+  let h = 0;
+  for (let i = 0; i < s.length; i++) {
+    const cc = s.charCodeAt(i);
+    h = (h * 23249425 + cc) % 24862048;
+  }
+  return h;
+}
+
+function getAuthorChipColor(author: string) {
+  const colorList = [
+    { color: "white", bgcolor: red[900] },
+    { color: "white", bgcolor: pink[900] },
+    { color: "white", bgcolor: purple[900] },
+    { color: "white", bgcolor: deepPurple[900] },
+    { color: "white", bgcolor: indigo[900] },
+    { color: "white", bgcolor: blue[900] },
+    { color: "white", bgcolor: lightBlue[900] },
+    { color: "white", bgcolor: cyan[900] },
+    { color: "white", bgcolor: teal[900] },
+    { color: "white", bgcolor: green[900] },
+    { color: "white", bgcolor: lightGreen[900] },
+    { color: "white", bgcolor: lime[900] },
+    { color: "white", bgcolor: yellow[900] },
+    { color: "white", bgcolor: amber[900] },
+    { color: "white", bgcolor: orange[900] },
+    { color: "white", bgcolor: deepOrange[900] },
+    { color: "white", bgcolor: brown[900] },
+    { color: "white", bgcolor: grey[900] },
+    { color: "white", bgcolor: blueGrey[900] },
+  ];
+  return colorList[hashString(author) % colorList.length];
+}
 
 function authorChips(authors: string[]) {
   // TODO padding or margine
   return (
     <div>
       {authors.map((a) => (
-        <Chip label={`${a}`} size="small" />
+        <Chip
+          label={`${a}`}
+          size="small"
+          sx={{
+            color: getAuthorChipColor(a).color,
+            bgcolor: getAuthorChipColor(a).bgcolor,
+          }}
+        />
       ))}
     </div>
   );
