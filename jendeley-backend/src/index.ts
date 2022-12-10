eec1a1311ce89d6b54e0cb7f2063038c37901098
Akira Kawata <akirakawata1@gmail.com>
2022-12-10 15:02:45 +0900

    Fix a bug

--- jendeley-backend/src/index.ts
+++ jendeley-backend/src/index.ts
@@ -26,10 +26,11 @@ async function main() {
         options._optionValues.papers_dir.slice(-1) == "/"
           ? options._optionValues.papers_dir
           : options._optionValues.papers_dir + "/";
+      const db_name = options._optionValues.db_name == undefined ? "db.json" : options._optionValues.db_name;
       genDB(
         pd,
         book_dirs_str,
-        options._optionValues.db_name,
+        db_name,
         options._optionValues.only_append
       );
     });
