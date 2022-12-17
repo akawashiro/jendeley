3ab006405f12a5ffdab8c6a8aac048f85620aecf
Akira Kawata <akirakawata1@gmail.com>
2022-12-17 21:40:18 +0900

    Refactor frontend

---
+++ jendeley-frontend/src/stringUtils.tsx
@@ -0,0 +1,61 @@
+import {
+  red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
+  blue,
+  lightBlue,
+  cyan,
+  teal,
+  green,
+  lightGreen,
+  lime,
+  yellow,
+  amber,
+  orange,
+  deepOrange,
+  brown,
+  grey,
+  blueGrey,
+} from "@mui/material/colors";
+
+function splitTagsStr(s: string) {
+  return s.split(",").filter((w) => w.length > 0);
+}
+
+function hashString(s: string) {
+  let h = 0;
+  for (let i = 0; i < s.length; i++) {
+    const cc = s.charCodeAt(i);
+    h = (h * 23249425 + cc) % 24862048;
+  }
+  return h;
+}
+
+function getColorFromString(author: string) {
+  const colorList = [
+    { color: "white", bgcolor: red[900] },
+    { color: "white", bgcolor: pink[900] },
+    { color: "white", bgcolor: purple[900] },
+    { color: "white", bgcolor: deepPurple[900] },
+    { color: "white", bgcolor: indigo[900] },
+    { color: "white", bgcolor: blue[900] },
+    { color: "white", bgcolor: lightBlue[900] },
+    { color: "white", bgcolor: cyan[900] },
+    { color: "white", bgcolor: teal[900] },
+    { color: "white", bgcolor: green[900] },
+    { color: "white", bgcolor: lightGreen[900] },
+    { color: "white", bgcolor: lime[900] },
+    { color: "white", bgcolor: yellow[900] },
+    { color: "white", bgcolor: amber[900] },
+    { color: "white", bgcolor: orange[900] },
+    { color: "white", bgcolor: deepOrange[900] },
+    { color: "white", bgcolor: brown[900] },
+    { color: "white", bgcolor: grey[900] },
+    { color: "white", bgcolor: blueGrey[900] },
+  ];
+  return colorList[hashString(author) % colorList.length];
+}
+
+export { splitTagsStr, hashString, getColorFromString };
