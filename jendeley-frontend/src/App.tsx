be14b5f6069b7cb5eec15c3b613a57f782996a93
Akira Kawata <akirakawata1@gmail.com>
2022-12-17 16:44:16 +0900

    Show dialogue when delete

--- jendeley-frontend/src/App.tsx
+++ jendeley-frontend/src/App.tsx
@@ -8,6 +8,7 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
+  DialogContentText,
   DialogTitle,
   IconButton,
   Stack,
@@ -81,43 +82,71 @@ function getColorFromString(author: string) {
   return colorList[hashString(author) % colorList.length];
 }
 
-async function deleteRow(id: string, tableData: any, setTableData: any) {
-  console.log("Delete " + id);
-
-  const e: Entry = {
-    abstract: "",
-    authors: [],
-    id: id,
-    title: "",
-    path: "",
-    tags: [],
-    comments: "",
-    year: 0,
-    publisher: "",
-  };
-  const response = await fetch("http://localhost:5000/api/delete_entry", {
-    method: "DELETE",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "PUT",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(e),
-  });
-  console.log("response of update_entry:", response);
-
-  fetch("http://localhost:5000/api/get_db")
-    .then((response) => response.json())
-    .then((json) => setTableData(() => json));
-}
-
 function DeleteButton(props: any) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  async function deleteRow(id: string, tableData: any, setTableData: any) {
+    console.log("Delete " + id);
+
+    const e: Entry = {
+      abstract: "",
+      authors: [],
+      id: id,
+      title: "",
+      path: "",
+      tags: [],
+      comments: "",
+      year: 0,
+      publisher: "",
+    };
+    const response = await fetch("http://localhost:5000/api/delete_entry", {
+      method: "DELETE",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "PUT",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(e),
+    });
+    console.log("response of update_entry:", response);
+
+    fetch("http://localhost:5000/api/get_db")
+      .then((response) => response.json())
+      .then((json) => setTableData(() => json));
+
+    setOpen(false);
+  }
+
   return (
-    <IconButton
-      onClick={() => deleteRow(props.id, props.tableData, props.setTableData)}
-    >
-      <Delete />
-    </IconButton>
+    <div>
+      <IconButton onClick={handleClickOpen}>
+        <Delete />
+      </IconButton>
+      <Dialog open={open}>
+        <DialogContent>
+          <DialogContentText>Delete "{`${props.title}`}"?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button
+            onClick={() =>
+              deleteRow(props.id, props.tableData, props.setTableData)
+            }
+            autoFocus
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 }
 
@@ -417,9 +446,10 @@ function App() {
     () => [
       {
         accessorKey: "id",
-        Cell: ({ cell }) => (
+        Cell: ({ cell, row }) => (
           <DeleteButton
             id={`${cell.getValue<string>()}`}
+            title={`${row.original.title}`}
             setTableData={setTableData}
             tableData={tableData}
           />
