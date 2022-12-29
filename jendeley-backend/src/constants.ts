6faec9b6090d33637a6deceb32da8482d3d4787e
Akira Kawata <akirakawata1@gmail.com>
2022-12-29 11:40:48 +0900

    Use defined constants

--- jendeley-backend/src/constants.ts
+++ jendeley-backend/src/constants.ts
@@ -1,4 +1,31 @@
 const JENDELEY_NO_TRACK = "[jendeley no track]";
 const JENDELEY_NO_ID = "[jendeley no id]";
+const ENTRY_ID_TYPE = "id_type";
+const ENTRY_PATH = "path";
+const ENTRY_URL = "url";
+const ENTRY_TAGS = "tags";
+const ENTRY_COMMENTS = "comments";
+const ENTRY_TITLE = "title";
+const ID_TYPE_ARXIV = "arxiv";
+const ID_TYPE_DOI = "doi";
+const ID_TYPE_ISBN = "isbn";
+const ID_TYPE_PATH = "path";
+const ID_TYPE_BOOK = "book";
+const ID_TYPE_URL = "url";
 
-export { JENDELEY_NO_TRACK, JENDELEY_NO_ID };
+export {
+  JENDELEY_NO_TRACK,
+  JENDELEY_NO_ID,
+  ENTRY_ID_TYPE,
+  ENTRY_PATH,
+  ENTRY_URL,
+  ENTRY_TAGS,
+  ENTRY_COMMENTS,
+  ENTRY_TITLE,
+  ID_TYPE_ARXIV,
+  ID_TYPE_DOI,
+  ID_TYPE_ISBN,
+  ID_TYPE_PATH,
+  ID_TYPE_BOOK,
+  ID_TYPE_URL,
+};
