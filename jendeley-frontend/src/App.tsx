742f62a23129b68180d15a7fb3211a12e8936fce
Akira Kawata <akirakawata1@gmail.com>
2023-01-10 22:34:09 +0900

    Delete unused code

--- jendeley-frontend/src/App.tsx
+++ jendeley-frontend/src/App.tsx
@@ -307,13 +307,4 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Failed to find the root element");
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
 export default App;
