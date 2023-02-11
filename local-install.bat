f41fa8ca03bc0de0dea17d441f9c4e10018a437f
Akira Kawata <akirakawata1@gmail.com>
2023-02-11 14:30:54 +0900

    Fix local install script

--- local-install.bat
+++ local-install.bat
@@ -1,10 +1,11 @@
 cd .\jendeley-frontend
+call npm install
 call npm run build
 cd ..
 
-del /q .\jendeley-backend\built-frontend
-copy /y .\jendeley-frontend\build .\jendeley-backend\built-frontend
+xcopy /E/H/Y .\jendeley-frontend\build .\jendeley-backend\built-frontend\
 
 cd .\jendeley-backend
+call npm install
 call npm run build
 call npm install . -g
