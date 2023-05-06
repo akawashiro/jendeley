61a2fb78562d3cd2874e930b93b7901f8e60c232
Akira Kawata <akirakawata1@gmail.com>
2023-05-06 22:55:14 +0900

    Version up to test

--- jendeley-backend/src/validate_db.ts
+++ jendeley-backend/src/validate_db.ts
@@ -35,10 +35,9 @@ function validateJsonDB(jsonDB: JsonDB, dbPath: string[] | undefined): boolean {
     const id_type = jsonDB[id][ENTRY_ID_TYPE];
     if (id_type == ID_TYPE_META) {
       const version = parseFloat(jsonDB[id]["version"]);
-      // Enable this check after version 2.0
-      // if (version < 2.0) {
-      //   validDB = false;
-      // }
+      if (version < 2.0) {
+        validDB = false;
+      }
       continue;
     }
 
