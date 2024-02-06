b937b0ec20d6eb697a875474663fa7a03814916f
Akira Kawata <akirakawata1@gmail.com>
2024-02-06 23:43:12 +0900

    Release v2.2.1

--- jendeley-backend/src/constants.ts
+++ jendeley-backend/src/constants.ts
@@ -1,4 +1,4 @@
-const JENDELEY_VERSION = "2.2.0";
+const JENDELEY_VERSION = "2.2.1";
 const JENDELEY_NO_TRACK = "[jendeley no track]";
 const JENDELEY_NO_ID = "[jendeley no id]";
 const ENTRY_ID_TYPE = "idType";
